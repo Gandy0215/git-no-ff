3ba0cc79db25fc8cfa6492ca25ac9c7a3c43e096
dongwoo <dongwoo@scgs.co.kr>
2021-06-09 17:29:19 +0900

    init

---
+++ src/TestClassMain.java
@@ -0,0 +1,13 @@
+
+public class TestClassMain {
+
+	public TestClassMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
